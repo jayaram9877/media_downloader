d47dc4438da652615539d907af4749c806423e43
jayaram mani kumar <ramjay457@gmail.com>
2020-04-11 11:13:26 +0530

    Downloads Page

---
+++ lib/Screens/Downloads.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class Downloads extends StatefulWidget {
+  @override
+  _DownloadsState createState() => _DownloadsState();
+}
+
+class _DownloadsState extends State<Downloads> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('DOWNLOADS'),),
+    );
+  }
+}
